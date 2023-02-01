38c9f047b2e9cb0f3173aeb867046de1d0b13906
Leandro-Ferreira-Nascimento <121138315+Leandro-Ferreira-Nascimento@users.noreply.github.com>
2023-02-01 19:42:14 -0300

    Update ler_arquivos_audio.py

--- ler_arquivos_audio.py
+++ ler_arquivos_audio.py
@@ -1,8 +1,10 @@
 import speech_recognition as sr
 
+# esse é no recognizer
 rec = sr.Recognizer()
 
+# lendo o arquivo de audio
 with sr.AudioFile("teste.wav") as arquivo_audio:
     audio = rec.record(arquivo_audio)
-    texto = rec.recognize_google(audio, language="pt-BR")
+    texto = rec.recognize_google(audio, language="en")
     print(texto)
